4320999516c6a21b2fe89de6879b249f8b559f0e
Santiago Aguilar Silva <48460335+saguilars@users.noreply.github.com>
2019-09-20 15:34:16 -0300

    Agregar configuracion de materias

---
+++ NCLASES/Controllers/MateriasController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NCLASES.Controllers
+{
+    public class MateriasController : Controller
+    {
+        // GET: Materias
+        public ActionResult Materias(string Menu)
+        {
+            ViewBag.Menu = Menu;
+            return View();
+        }
+    }
+}
---
+++ NCLASES/Controllers/TareasController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NCLASES.Controllers
+{
+    public class TareasController : Controller
+    {
+        // GET: Tareas
+        public ActionResult Tareas()
+        {
+            return View();
+        }
+    }
+}
